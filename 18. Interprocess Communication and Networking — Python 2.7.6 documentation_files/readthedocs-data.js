61100674596147761ae4169176dd5a7f886c84b0
Pasha <43281113+holographics@users.noreply.github.com>
2019-02-23 16:20:13 -0800

    Add files via upload

---
+++ 18. Interprocess Communication and Networking — Python 2.7.6 documentation_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "cpython-test-docs",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://cpython-test-docs.readthedocs.io/en/latest/",
+    theme: "pydoctheme",
+    builder: "sphinx",
+    docroot: "/./",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "56fba1f2"
+  };
+  
